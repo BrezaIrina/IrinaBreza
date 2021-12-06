b6e66b0c021cc23d97c5e5f1ac1734b52b5c623d
Irina Breza <irka.demyan@gmail.com>
2021-12-06 16:04:44 +0200

    IB-10 Added exception with handling to PokerGame

--- HillelCourcesQAA/src/com/irinabreza/hillel/Homework6/PokerGame.java
+++ HillelCourcesQAA/src/com/irinabreza/hillel/Homework6/PokerGame.java
@@ -7,22 +7,32 @@ public class PokerGame {
         Player anton = new Player("Anton");
         Player tom = new Player("Tom");
         Player pit = new Player("Pit");
-
         Player[] players = {ivan, anton, tom, pit};
 
-        Deck shuffledDeck = new Deck(Card.allRanks, Card.allSuits);
-        System.out.println("Game starts");
-
-        for(int index = 0; index < 5; index++) {
-            System.out.println("");
-            System.out.println("New round");
-            for (Player currentPlayer : players) {
-                Card currentCard = shuffledDeck.getRandomCard();
-                String currentCardDescription = currentCard.getDescription();
-                System.out.printf("Player %s receives %s", currentPlayer.name, currentCardDescription);
-                System.out.println("");
-            }
-        }
+        tryToPlayGame(players);
         System.out.println("Game ends");
+    }
+
+    private static void tryToPlayGame(Player[] players) {
+        try {
+            Deck shuffledDeck = new Deck(Card.emptyArray, Card.allSuits);
+            System.out.println("Game starts");
+            for (int index = 0; index < 15; index++) {
+                System.out.println("");
+                System.out.println("New round");
+                for (Player currentPlayer : players) {
+                    drawRandomCard(shuffledDeck, currentPlayer);
+                }
+            }
+        } catch (PokerGameException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    private static void drawRandomCard(Deck shuffledDeck, Player currentPlayer) throws PokerGameException {
+        Card currentCard = shuffledDeck.getRandomCard();
+        String currentCardDescription = currentCard.getDescription();
+        System.out.printf("Player %s receives %s", currentPlayer.name, currentCardDescription);
+        System.out.println("");
     }
 }
