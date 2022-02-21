1fa29e50af1a86a54d36018843346e019f02108d
Ira <irka.demyan@gmail.com>
2022-02-21 15:12:50 +0200

    Updated pom.xml and BaseTest for running via Jenkins

--- HillelCourcesQAA/src/test/java/ua/hillel/test/wdTest/BaseTest.java
+++ HillelCourcesQAA/src/test/java/ua/hillel/test/wdTest/BaseTest.java
@@ -4,16 +4,54 @@ import com.irinabreza.hillel.pageObjects.MainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeClass
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setUp(String browser){
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+            Map<String, Object> prefs = new HashMap<String, Object>();
+            prefs.put("download.default_directory", new File("target/download").getAbsolutePath());
+            options.setExperimentalOption("prefs", prefs);
+
+            this.driver = new ChromeDriver(options);
+        }
+
+        if (browser.equals("remote")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", "chrome");
+            capabilities.setCapability("browserVersion", "98.0");
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", false
+            ));
+            try {
+                driver = new RemoteWebDriver(
+                        URI.create("http://192.168.4.42:4444/wd/hub").toURL(),
+                        capabilities
+                );
+
+            } catch (MalformedURLException m) {
+                m.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
